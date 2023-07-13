a19b57a5ad4b24879ad711842dfa4572d0e5f78f
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-07-13 00:23:34 -0400

    still working on my board game

--- scipt.js
+++ scipt.js
@@ -1,29 +1,32 @@
-const gameSection = document.getElementById("game-section");
 
-let currentPlayer = 'X';
-let gameBoard = Array(9).fill('');
- 
 function createGameBoard() {
-    gameSection.innerHTML = '';
+    let gameSection = document.querySelector("game-section");
+  for (let i = 0; i < 9; i++) {
+    let square = document.createElement('div');
+    square.classList.add('square', 'empty');
 
-    for(let i=0; i<9; i++) {
-        const square = document.createElement('div');
-        square.classList.add("square", "empty");
-        square.addEventListener('click', () => squareClicked(i));
-        gameSection.appendChild(square);
+    square.addEventListener('click',function() { if(this.classList.contains("empty")){
+        console.log("square is empty");
+        makeMoves(this);
+
+    }else{
+        console.log("square is not empty");
     }
+});
+gameSection.appendChild(square); 
+
+
+
+function resetGame() {
+  let squares = gameSection.querySelectorAll ('square');
+ squares.forEach(square => {
+     square.classList.add('empty');
+     square.textContent = '';
+  });
+  currentPlayer = 'X';
+  gameSection.textContent = '';
 }
 
-function squareClicked(index){
-    if(gameBoard[index]=== ""){
-        gameBoard[index] = currentPlayer;
-        const squares = gameSection.getElementsByClassName('square');
-        const square = squares[index];
-        square.textContent = currentPlayer;
-        square.classList.remove('empty');
+button.addEventListener('click', resetGame);
 
-        currentPlayer = currentPlayer === 'X'? 'O' : 'X';
-        gameSection.textContent = `Player ${currentPlayer}'s turn`; 
-    }
-}
 createGameBoard();
