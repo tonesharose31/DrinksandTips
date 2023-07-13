29dc74f29b31491130aa99aad2dd5b497610a275
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-07-12 23:12:41 -0400

    created a tic-tac-toe board as the game

--- scipt.js
+++ scipt.js
@@ -1,1 +1,29 @@
+const gameSection = document.getElementById("game-section");
 
+let currentPlayer = 'X';
+let gameBoard = Array(9).fill('');
+ 
+function createGameBoard() {
+    gameSection.innerHTML = '';
+
+    for(let i=0; i<9; i++) {
+        const square = document.createElement('div');
+        square.classList.add("square", "empty");
+        square.addEventListener('click', () => squareClicked(i));
+        gameSection.appendChild(square);
+    }
+}
+
+function squareClicked(index){
+    if(gameBoard[index]=== ""){
+        gameBoard[index] = currentPlayer;
+        const squares = gameSection.getElementsByClassName('square');
+        const square = squares[index];
+        square.textContent = currentPlayer;
+        square.classList.remove('empty');
+
+        currentPlayer = currentPlayer === 'X'? 'O' : 'x';
+        gameSection.textContent = `Player ${currentPlayer}'s turn`; 
+    }
+}
+createGameBoard();
