81442bc114c227442346a6730030cbc6185db8e7
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-07-19 06:58:47 -0400

    created a js_file to handle the 2nd html page

---
+++ contact.js
@@ -0,0 +1,8 @@
+const commentaryForm= document.querySelector("#commentaryForm");
+commentaryForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+const name = document.getElementById("nameInput").value;
+const commentary = document.getElementById("commentaryInput").value
+
+})
