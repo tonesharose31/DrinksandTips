e8dd41ae7fca022928fcf4fdf45c4e21174b2aa8
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-07-19 07:22:07 -0400

    created a function to post comments to page

--- contact.js
+++ contact.js
@@ -5,4 +5,14 @@ commentaryForm.addEventListener("submit", (e) => {
 const name = document.getElementById("nameInput").value;
 const commentary = document.getElementById("commentaryInput").value
 
-})
+const commentElement = document.createElement("div");
+commentElement.classList.add("comment");
+commentElement.innerHTML = `
+<strong>${name}</strong>
+<p>${commentary}</p>
+<hr>`;
+
+commentsContainer.appendchild(commentElement);
+document.getElementById("nameInput").value="";
+document.getElementById("commentaryInput").value="";
+});
